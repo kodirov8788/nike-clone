c3ca3fb19f870593bde8af3720f5fb7fad57a65d
Kodirov8788@gmail.com <kodirov8788@gmail.com>
2021-08-18 14:48:47 +0500

    added carousel

--- src/components/carousel/Caroucel.js
+++ src/components/carousel/Caroucel.js
@@ -1,33 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./carousel.css";
 import { BsArrowBarLeft, BsArrowBarRight } from "react-icons/bs";
 
 function Caroucel() {
-  const handleleftbtn = () => {
+  // const handleleftbtn = () => {
+  //   const text1 = document.querySelector(".text1");
+  //   const text2 = document.querySelector(".text2");
+  //   text1.style.transform = "translate(-4000px)";
+  //   text2.style.transform = "translate(0)";
+  // };
+  // const handlerightbtn = () => {
+  //   const text1 = document.querySelector(".text1");
+  //   const text2 = document.querySelector(".text2");
+  //   text1.style.transform = "translate(0)";
+  //   text2.style.transform = "translate(4000px)";
+  // };
+
+  const [text, setText] = useState("first");
+
+  useEffect(() => {
     const text1 = document.querySelector(".text1");
     const text2 = document.querySelector(".text2");
-    text1.style.transform = "translate(-4000px)";
-    text2.style.transform = "translate(0)";
-  };
-  const handlerightbtn = () => {
-    const text1 = document.querySelector(".text1");
-    const text2 = document.querySelector(".text2");
-    text1.style.transform = "translate(0)";
-    text2.style.transform = "translate(4000px)";
-  };
+
+    setTimeout(() => {
+      if (text === "text1") {
+         text2.style.transform = "translate(650px)";
+         text1.style.transform = "translate(-4000px)";
+        setText("text2");
+      } else {
+        text2.style.transform= "translate(4000px)";
+        text1.style.transform = "translate(650px)";
+
+        setText("text1");
+      }
+    }, 5000);
+  }, [text]);
 
   return (
     <div className="carousel">
-      <div className="left__arrow" onClick={handleleftbtn}>
+      {/* <div className="left__arrow" onClick={handleleftbtn}>
         <BsArrowBarLeft />
-      </div>
+      </div> */}
+
       <div className="text__container">
-        <h1 className="text1">Text 1</h1>
-        <h1 className="text2">Text 2</h1>
+        <div className="text1">
+          <h1>Text 1</h1>
+        </div>
+        <div className="text2">
+          <h1>Text 2</h1>
+        </div>
       </div>
-      <div className="right__arrow" onClick={handlerightbtn}>
+      {/* <div className="right__arrow" onClick={handlerightbtn}>
         <BsArrowBarRight />
-      </div>
+      </div> */}
     </div>
   );
 }
